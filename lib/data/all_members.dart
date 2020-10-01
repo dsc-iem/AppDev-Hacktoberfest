1efd689443249aab4079d8f496ede7f684ceb76f
Nibba2018 <sohambiswas41@gmail.com>
2020-10-01 13:25:57 +0530

    update profile

--- lib/data/all_members.dart
+++ lib/data/all_members.dart
@@ -94,6 +94,18 @@ class AllMembers {
         linkedInUrl: 'https://www.linkedin.com/in/omprakash-kumar-0237161b0/',
         status: 'Always Learning'),
 
+    Member(
+        name: 'Soham Biswas',
+        year: '3rd',
+        department: 'Computer Science',
+        profilePath: 'assets/profiles/soham.jpeg',
+        aboutMe:
+            'Machine Learning and Computational Science Enthusiast, who loves contributing to Open Source and prefers to be in the R&D team.',
+        fburl: '',
+        githubUrl: 'https://github.com/Nibba2018',
+        linkedInUrl: 'https://www.linkedin.com/in/soham-biswas-590784168/',
+        status: 'Squashing Bugs'),
+
   ];
 
   static List<Member> get members {
